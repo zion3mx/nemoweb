923fe52c6c52839f9cab5c7563ba228089e912cc
zion3mx <zion3mx@gmail.com>
2016-10-21 13:42:42 +0900

    Update SummaryRuntimException.java

--- SummaryRuntimException.java
+++ SummaryRuntimException.java
@@ -2,7 +2,8 @@
 1. Checked Exception Spring Transaction에서 자동 RollBack하지 않기 때문에 필요시 Rollback-for="SqlException"(만 Exception으로 지정시 기대하지 않은 곳에서 롤백)
  - Spring JDBC, MyBatis와 같은 곳에서는 SQLException(Checked Exception)을 발생하지않고 RuntimeException 발생
  - Spring Framework에서 SQLExcetption과 같은 Checked Exception 남용으로 인한 피해를 줄이고자 DataAccessException(RunTimeException) 포장해서 제공
- - 프레임워크에서도 가능하면 Checked Exception을 사용하지 않는 방향으로 진행
+
+2. Spring, Hiberate 프레임워크에서도 가능하면 Checked Exception을 사용하지 않는 방향으로 진행
 */
 
 
